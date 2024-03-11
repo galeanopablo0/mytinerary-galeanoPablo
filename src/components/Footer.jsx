15e11b37c80484658ff4e93a5a631487592b7ec9
galeanopablo0 <galeanopablo229@gmail.com>
2024-03-11 17:50:21 -0300

    footer responsive y un poco de estilo añadido

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,8 +4,8 @@ import { Link } from 'react-router-dom'
 const Footer = () => {
   return (
     <>
-        <footer className='flex flex-col items-center gap-5 p-9'>
-            <nav className='flex gap-10'>
+        <footer className='flex flex-col items-center gap-5 p-9 border-t-2 border-black'>
+            <nav className='flex flex-wrap justify-center gap-10'>
               <Link to="/whatsapp"><i className='fa-brands fa-whatsapp scale-150 pr-[5px]'></i>Whatsapp</Link>
               <Link to="/instagram"><i className='fa-brands fa-instagram scale-150 pr-[5px]'></i>Instagram</Link>
               <Link to="/facebook"><i className='fa-brands fa-facebook scale-150 pr-[5px]'></i>Facebook</Link>
