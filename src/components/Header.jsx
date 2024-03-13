acb569b21a7c57a1071967d64627fbe519318dda
galeanopablo0 <galeanopablo229@gmail.com>
2024-03-13 15:46:55 -0300

    <a> arreglado en header, reemplazado por <Link>

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -6,7 +6,7 @@ const Header = () => {
       <>
         <header className='bg-gray-50 flex justify-center'>
             <div className='flex justify-between p-5 py- md:py-10 items-center w-[95%] sm:w-[80%]'>
-              <a href=""><h1 className='text-2xl md:text-3xl font-bold flex align-center'>My Tinerary</h1></a>
+              <h1><Link to={'/'} className='text-2xl md:text-3xl font-bold flex align-center'>My Tinerary</Link></h1>
               <nav className='flex items-center gap-6 lg:gap-10'>
                 <Link className='text-xl hover:scale-95 hover:text-gray-600' to="/">Home</Link>
                 <Link className='text-xl hover:scale-95 hover:text-gray-600' to="/cities">Cities</Link>
